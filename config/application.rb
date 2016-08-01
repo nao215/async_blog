6c6167a84779c26a5d71d0f227e31b13f8969634
YanagisawaNao <Alexander@Alexander.local>
2016-08-01 21:23:11 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module AsyncBlog
+  class Application < Rails::Application
+
+		config.assets.enabled = false
+
+  end
+end
