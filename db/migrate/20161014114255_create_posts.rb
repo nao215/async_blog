b295e18f5167d58e4e95ec937ae3a9cc724ce9de
柳澤 直 <n_yanagisawa@r.recruit.co.jp>
2016-10-14 21:22:30 +0900

    scaffold post

---
+++ db/migrate/20161014114255_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :text
+      t.string :slug
+      t.string :state
+
+      t.timestamps
+    end
+  end
+end
